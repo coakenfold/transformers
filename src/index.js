7e8992eb6c501adceaae5c8a375d7575b5bba370
Chad Oakenfold <web@oakenfold.ca>
2018-07-13 14:02:29 -0700

    moving index.js to src folder and spinning up react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const createTeams = require('./src/createTeams')
+/*const createTeams = require('./src/createTeams')
 const battler = require('./src/battler')
 const generateOutput = require('./src/generateOutput')
 
@@ -21,3 +21,10 @@ const battles = battler(teams.a, teams.d)
 const result = battles.battle()
 const output = generateOutput(result)
 console.log(output)
+*/
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var App = require('./components/App')
+
+ReactDOM.render(<App />, document.getElementById('app'))
