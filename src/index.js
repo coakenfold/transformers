ba5afd29f265ce623f78d8b8da301e7658fac82a
Chad Oakenfold <web@oakenfold.ca>
2018-07-13 16:19:58 -0700

    Simpler index.html

--- src/index.js
+++ src/index.js
@@ -27,4 +27,4 @@ var ReactDOM = require('react-dom')
 
 var App = require('./components/App')
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('appContainer'))
