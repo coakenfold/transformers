636fa0eef978badae6c6b2c583c3994e9042cf30
Chad Oakenfold <web@oakenfold.ca>
2018-07-13 14:02:45 -0700

    Roughed in App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import style from './App.css'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div className="App">React up and running</div>
+  }
+}
+
+module.exports = App
